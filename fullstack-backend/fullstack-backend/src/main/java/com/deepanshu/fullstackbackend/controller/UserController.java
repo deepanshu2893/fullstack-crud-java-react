a457e2f1f3109da9890c298d0fdbfc49226a929e
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-03-26 23:25:52 +0530

    Update UserController.java

--- fullstack-backend/fullstack-backend/src/main/java/com/deepanshu/fullstackbackend/controller/UserController.java
+++ fullstack-backend/fullstack-backend/src/main/java/com/deepanshu/fullstackbackend/controller/UserController.java
@@ -13,7 +13,7 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/use")
+    @PostMapping("/user")
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
     }
